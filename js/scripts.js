6b29854cf43be031d4131790e6c0e6fe911f37ea
Dushyant Kumar Behera <49820671+DarkNDev@users.noreply.github.com>
2023-08-07 19:36:50 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#intro', {
+    strings: ['Hello, My name is <span id="element_color">Dushyant</span> <br>and I am a passionate <br><span id="element_color">Android Developer</span>'],
+    typeSpeed: 50,
+});
