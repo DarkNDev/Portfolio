96a20aa0c918cfa71ba7121ae5d62ef88c215ce1
Dushyant Kumar Behera <49820671+DarkNDev@users.noreply.github.com>
2023-08-09 04:06:06 +0530

    portfolio using tailwind css

--- js/scripts.js
+++
@@ -1,4 +0,0 @@
-var typed = new Typed('#intro', {
-    strings: ['Hello, My name is <span id="element_color">Dushyant</span> <br>and I am a passionate <br><span id="element_color">Android Developer</span>'],
-    typeSpeed: 50,
-});
